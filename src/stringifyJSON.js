e0357c6e951121512fada3d36f64f1ae7f812535
Andy Chan <Chanandy727@gmail.com>
2022-02-22 16:48:12 -0800

    lmost done

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,18 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //if undefined
+  if (typeof obj === 'undefined') {
+    return undefined;
+  } else if (typeof obj === 'number') {
+    return '' + obj;
+  } else if (typeof obj === 'string') {
+    return obj;
+  } else if (Array.isArray(obj)) {
+    return '[' + _(obj).map(stringifyJSON).join(',') + ']';
+  }
 };
+
+// btw could u give me the clone link to ur fork of recursion-review please?
+// could paste it here while the liveShare is active
+// or DM in Slack (:
